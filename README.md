d70d068ae20c68b72bfb1335f7a1f96eb353d4fb
Dmitry Kolesov <kolesov.dm@gmail.com>
2016-06-28 10:36:39 +0300

    Импорт низкоуровневых бииблиотек работы с растрами + описание метода обхода ошибки импорта

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # remote_sensing
-Remote sensing tests
+
+Здесь собраны блокноты IPython (Jupyter), которые используются для документирования экспериментов в области ДЗЗ.
+
+
+## Замечания по установке
+
+Для запуска GRASS из блокнотов (использования модуля grasslib.py) необходимо предварительно установить переменную export `LD_LYBRARY_PATH` **до** вызова `Jupyter`, иначе при импорте возникнет ошибка связывания библиотек, т.к. установка этой переменной в ходе исполнения не работает (см. http://stackoverflow.com/questions/856116/changing-ld-library-path-at-runtime-for-ctypes):
+```
+export LD_LIBRARY_PATH=/usr/lib/grass70/lib
+```
+
+После установки переменной можно запускать `Jupyter`:
+```
+jupyter notebook
+```
+Или в фоновом режиме:
+```
+jupyter notebook 2> /dev/null > /dev/null &
+```
--- deforestation/grasslib.py
+++ deforestation/grasslib.py
@@ -6,7 +6,8 @@ import sys
 class GRASS():
     def __init__(self, gisbase, dbase, location, mapset='PERMANENT'):
 
-        self.gisbase = os.environ['GISBASE'] = gisbase
+        self.gisbase = os.environ['GISBASE'] = gisbase 
+        os.environ['LD_LIBRARY_PATH'] = os.path.join(self.gisbase, 'lib')
         self.dbase = dbase
 
         self.location = location
@@ -15,15 +16,19 @@ class GRASS():
         # Пока не обновлены пути, импортировать GRASS не удастся
         sys.path.append(os.path.join(os.environ['GISBASE'], "etc", "python"))
         
-        import grass.script as grass
         import grass.script.setup as gsetup
-
-        self.grass = grass
         self.gsetup = gsetup
 
         # Активируем GRASS
         self.gsetup.init(self.gisbase,
-            self.dbase, self.location, self.mapset)
+            self.dbase, self.location, self.mapset)        
+        
+        # Импортируем полезные модули
+        import grass.script as grass
+        import grass.pygrass.raster as grs_raster
+
+        self.grass = grass
+        self.r = grs_raster
 
     def get_region_info(self):
         gregion = self.grass.region()
@@ -31,5 +36,5 @@ class GRASS():
 
 
 if __name__ == "__main__":
-    grs = GRASS(gisbase='/usr/lib/grass70', dbase='/home/dima/GRASSDATA', location='AML')
+    grs = GRASS(gisbase='/usr/lib/grass70', dbase='/home/cruncher/GRASSDATA', location='FOREST')
     grs.grass.run_command('g.region', flags='p')
