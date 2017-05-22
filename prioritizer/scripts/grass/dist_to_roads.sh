8f7290c2a9278fb97dd4f0dbc38f45e4dcab116a
Dmitry Kolesov <kolesov.dm@gmail.com>
2017-05-22 15:32:57 +0300

    drake-скрипты расчета расстояний до дорог

---
+++ prioritizer/scripts/grass/dist_to_roads.sh
@@ -0,0 +1,15 @@
+
+set -e
+
+COST_MAP=tmp_cost_map
+COST_VALUE=$(g.region -g | grep nsres | cut -d= -f2)
+
+r.mapcalc expression="$COST_MAP = $COST_VALUE"
+
+for IN_MAP in asfalt good_grunt grunt land other trop
+do
+    OUT_MAP=dist_$IN_MAP
+    r.cost input=$COST_MAP output=$OUT_MAP start_raster=$IN_MAP --o
+done
+
+g.remove type=rast name=$COST_MAP -f
